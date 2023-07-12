efcde3b75947ff0a81d458a3f30fda622a83df5a
starboyririi <owen.riri@strathmore.edu>
2023-07-12 16:05:51 +0300

    changes about the about page

--- about.php
+++ about.php
@@ -18,16 +18,20 @@ session_start();
 <body>
   <header>
     <h2 class="logo">
-      <img src="images/SCH 2.jpg" alt="S.C.H logo" width="100px" height="80px" border-radius="50px" 6px; />
+      <img src="images/SCH 2.jpg" alt="S.C.H logo" width="100px" height="80px" style="border-radius: 50px; margin-right: 6px;" />
     </h2>
     <nav class="navigation">
       <a href="index.php">Home</a>
-      <a href="about.php">About</a>
       <a href="services.php">Services</a>
       <a href="contact.php">Contact</a>
       <button class="btnLogin-popup">Login</button>
     </nav>
   </header>
+  <section>
+  <h1>About SunCity Hospital:</h1>
+    <h5>SunCity Hospital is a leading healthcare facility dedicated to providing exceptional medical care and services to the community. With state-of-the-art facilities and a team of highly skilled healthcare professionals, we strive to deliver the highest standard of patient-centered care.</h5>
+
+  </section>
 </body>
 
 </html>
